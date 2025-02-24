55e830052d94485107a35f9e3f7dd6f85ae17a6d
Paravada Naveen teja <dora.naveen@gmail.com>
2025-02-23 22:28:54 -0600

    useTimer Helper Initial upload

---
+++ useTimer/useTimer.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+export function useTimer() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => setElapsedTime((prevElapsedTime) => prevElapsedTime + 1), 1000);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  return {
+    isRunning,
+    setIsRunning,
+    elapsedTime,
+    setElapsedTime,
+  };
+}
+
+export const useStopwatch = () => {
+  const [laps, setLaps] = useState([]);
+  const { isRunning, setIsRunning, elapsedTime, setElapsedTime } = useTimer();
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+    setLaps([]);
+  };
+
+  const handleAddLap = () => {
+    const prevTotal = laps.length > 0 ? laps.reduce((acc, curr) => acc + curr, 0) : 0;
+    const currentLap = laps.length > 0 ? elapsedTime - prevTotal : elapsedTime;
+    isRunning && setLaps([...laps, currentLap]);
+  };
+
+  return {
+    elapsedTime: elapsedTime,
+    laps,
+    addLap: () => handleAddLap(),
+    resetTimer: () => handleReset(),
+    startTimer: () => setIsRunning(true),
+    stopTimer: () => setIsRunning(false),
+    isRunning,
+  };
+};
